65d28fda505f6ef119c266dd555f1201f7f99d0d
jonah791 <jonah791@users.noreply.github.com>
2026-05-12 11:22:25 +0800

    fix: disable resource shrinking too (required by ABI splits)

--- android/app/build.gradle.kts
+++ android/app/build.gradle.kts
@@ -40,6 +40,7 @@ android {
     buildTypes {
         release {
             isMinifyEnabled = false
+            isShrinkResources = false
             signingConfig = signingConfigs.getByName("debug")
         }
     }
